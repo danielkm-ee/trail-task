073a188aec8847e1291e3241b1a8510a6fae977c
dkm-ee <dannym1432@gmail.com>
2024-07-19 00:03:50 -0700

    Implemented toroidal canvas and edgecases, as well as a function to load the santa fe trail by pressing 's'

--- game.py
+++ game.py
@@ -16,18 +16,19 @@ SCALE = 20
 WIDTH = GRID_SIZE * SCALE
 HEIGHT = GRID_SIZE * SCALE
 
+TOROIDAL_MAP = True     # if true edges, are wrapped around to eachother. otherwise edges are walls
+
+SANTA_FE = [[0.0, 0.0], [20.0, 0.0], [40.0, 0.0], [60.0, 0.0], [60.0, 20.0], [60.0, 40.0], [60.0, 60.0], [60.0, 80.0], [60.0, 100.0], [80.0, 100.0], [100.0, 100.0], [120.0, 100.0], [160.0, 100.0], [180.0, 100.0], [200.0, 100.0], [220.0, 100.0], [240.0, 100.0], [240.0, 120.0], [240.0, 140.0], [240.0, 160.0], [240.0, 180.0], [240.0, 200.0], [240.0, 240.0], [240.0, 260.0], [240.0, 280.0], [240.0, 300.0], [240.0, 360.0], [240.0, 380.0], [240.0, 400.0], [240.0, 420.0], [240.0, 440.0], [240.0, 460.0], [220.0, 480.0], [200.0, 480.0], [180.0, 480.0], [160.0, 480.0], [140.0, 480.0], [100.0, 480.0], [80.0, 480.0], [40.0, 500.0], [40.0, 520.0], [40.0, 540.0], [40.0, 560.0], [60.0, 600.0], [80.0, 600.0], [100.0, 600.0], [120.0, 600.0], [160.0, 580.0], [160.0, 560.0], [180.0, 540.0], [200.0, 540.0], [220.0, 540.0], [240.0, 540.0], [260.0, 540.0], [280.0, 540.0], [300.0, 540.0], [340.0, 520.0], [340.0, 500.0], [340.0, 480.0], [340.0, 420.0], [340.0, 380.0], [340.0, 360.0], [340.0, 340.0], [360.0, 320.0], [420.0, 300.0], [420.0, 280.0], [420.0, 220.0], [420.0, 200.0], [420.0, 180.0], [420.0, 160.0], [440.0, 100.0], [460.0, 100.0], [500.0, 80.0], [500.0, 60.0], [520.0, 40.0], [540.0, 40.0], [560.0, 40.0], [580.0, 60.0], [580.0, 80.0], [580.0, 120.0], [580.0, 180.0], [580.0, 240.0], [560.0, 280.0], [540.0, 280.0], [520.0, 280.0], [460.0, 300.0], [480.0, 360.0], [540.0, 380.0], [520.0, 440.0], [460.0, 460.0]]
+
 # colors
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
-gray = pygame.Color(100, 100, 100)
+gray = pygame.Color(150, 150, 150)
 red = pygame.Color(125, 10, 10)
-lightyellow = pygame.Color(250, 245, 170)
+lightyellow = pygame.Color(250, 245, 200)
 
 agent = pygame.image.load('agent.png')
 agent = pygame.transform.scale(agent, (WIDTH//GRID_SIZE, HEIGHT//GRID_SIZE))
-pygame.font.init()
-font = pygame.font.Font(pygame.font.get_default_font(), 22)
-txt = font.render('⮝', True, white)
 
 class Ant:
     def __init__(self, position=[0, 0]):
@@ -47,19 +48,32 @@ class Ant:
             raise ValueError
             print("Ant.dir is out of range!")
 
-
     def draw(self):
         sprite = pygame.transform.rotate(agent, self.dir * (360/4))
         map_.surface.blit(sprite, (self.position[0], self.position[1]) )
 
 
 class Map:
-    def __init__(self, width=WIDTH, height=HEIGHT, color=lightyellow):
+    def __init__(self, width=WIDTH, height=HEIGHT, color=lightyellow, toroidal=TOROIDAL_MAP):
         self.width = width
         self.height = height
         self.color = color
+        self.toroidal = toroidal
 
         self.surface = pygame.display.set_mode((self.width, self.height))
+    
+    def handleEdges(self):
+        if ant.position[0] >= self.width:
+            ant.position[0] = 0 if self.toroidal else self.width - self.width/GRID_SIZE
+
+        if ant.position[0] < 0:
+            ant.position[0] = self.width - self.width/GRID_SIZE if self.toroidal else 0
+
+        if ant.position[1] >= self.height:
+            ant.position[1] = 0 if self.toroidal else self.height - self.height/GRID_SIZE
+        
+        if ant.position[1] < 0:
+            ant.position[1] = self.height - self.height/GRID_SIZE if self.toroidal else 0
 
     def draw(self):
         self.surface.fill(self.color)
@@ -69,20 +83,38 @@ class Map:
                 pygame.draw.line(self.surface, gray, (x, 0), (x, self.height), 2)
                 pygame.draw.line(self.surface, gray, (0, y), (self.width, y), 2)
 
+
 class Pellet:
     def __init__(self, position=[WIDTH/GRID_SIZE * 10, HEIGHT/GRID_SIZE * 10]):
         self.position = position
         self.wasEaten = False
 
     def draw(self):
-        pygame.draw.rect(map_.surface, red, pygame.Rect(self.position[0], self.position[1], WIDTH / GRID_SIZE, HEIGHT / GRID_SIZE))
+        pygame.draw.rect(map_.surface, red, pygame.Rect(self.position[0], self.position[1], 
+                                                        WIDTH / GRID_SIZE, HEIGHT / GRID_SIZE))
 
 class Trail:
     def __init__(self):
         self.pellets = list()
     
-    def addPellet(position):
-       return
+    def addPellet(self, position):
+        pellet = Pellet(position)
+        self.pellets.append(pellet)
+
+    def load(self, trailname):
+        if trailname == "santa fe":
+            for pos in SANTA_FE:
+                self.addPellet(pos)
+
+
+    def update(self):
+        for i in range(len(self.pellets)):
+            if self.pellets[i-1].position == ant.position:
+               self.pellets.pop(i-1)
+
+    def draw(self):
+        for i in range(len(self.pellets)):
+            self.pellets[i].draw()
 
 
 # Initialising pygame
@@ -101,30 +133,46 @@ def rlm_controls(ant, key):
         ant.move()
     return
 
+def pellet_on_click():
+    mousePosition = pygame.mouse.get_pos()
+    xPos = mousePosition[0] - mousePosition[0] % (WIDTH / GRID_SIZE);
+    yPos = mousePosition[1] - mousePosition[1] % (HEIGHT / GRID_SIZE);
+    trail.addPellet([xPos, yPos]);
+
+
 font = pygame.font.Font
 ant = Ant();
 map_ = Map();
-pellet = Pellet();
+trail = Trail();
 
 while True:
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             rlm_controls(ant, event.key)
+
+            if event.key == pygame.K_s: # load santa fe trail
+                trail.load("santa fe")
+
             if event.key == pygame.K_q:
                 pygame.quit()
                 quit()
+
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pellet_on_click()
 
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
     
+    map_.handleEdges()
     map_.draw()
-    pellet.draw()
+    trail.draw()
     ant.draw()
 
-  
-
     pygame.display.update()
+
+    trail.update()
 
